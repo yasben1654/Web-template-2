5fa12247375019b398c0d3ffa983e219750dfc0a
yasben1654 <yassir.nl22@gmail.com>
2024-11-22 00:24:10 +0100

    font 2

--- src/util/includeScripts.js
+++ src/util/includeScripts.js
@@ -146,9 +146,14 @@ export const IncludeScripts = props => {
       }
     }
   };
-const fontscript = (<style>
-  ['@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');']
-  </style>);
+  const fontscript = () => (
+    <style>
+      {`@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Faculty+Glyphic&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&display=swap');`}
+    </style>
+  );
+  
+
+
 
   const allScripts = [...analyticsLibraries, ...mapLibraries, fontscript];
   return <Helmet onChangeClientState={onChangeClientState}>{allScripts}</Helmet>;
